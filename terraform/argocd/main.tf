6f3fc671a291e13dc4b5fce6727077e1885359b8
Ramiro Blanco <r@miroblan.co>
2023-06-11 12:42:23 -0300

    refactoring

--- terraform/argocd/main.tf
+++ terraform/argocd/main.tf
@@ -34,6 +34,12 @@ provider "helm" {
   }
 }
 
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.primary.endpoint}"
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  token                  = data.google_client_config.current.access_token
+}
+
 data "google_client_config" "current" {}
 
 data "google_container_cluster" "primary" {
