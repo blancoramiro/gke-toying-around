d8233a7e1eec1cb7866dd1a7ff500caef59e7444
Ramiro Blanco <r@miroblan.co>
2023-06-11 14:56:19 -0300

    refactoring

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,11 @@ output "gcloud_connect" {
   description = "gcloud cli add cluster"
   value       = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region=${google_container_cluster.primary.location}"
 }
+output "cluster_name" {
+  description = "cluster name"
+  value       = google_container_cluster.primary.name
+}
+output "cluster_location" {
+  description = "cluster location"
+  value       = google_container_cluster.primary.location
+}
